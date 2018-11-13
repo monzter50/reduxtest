b2f7822aea582776052cc0e5c422ce99a25821ab
monzter50 <josemonzter50@gmail.com>
2018-11-12 21:04:42 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
+import {createStore} from 'redux';
 import logo from './logo.svg';
 import './App.css';
-
+// import { composeWithDevTools } from ‘redux-devtools-extension;
+const person = ["Jose Antonio","Jesus Antonio","Maria Jose","Mariela Gonzalez"]
+const store = createStore(()=>{},window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { 
+      person:[]
+    }
+    this.hanglePushPerson = this.hanglePushPerson.bind(this);
+  }
+  hanglePushPerson(){
+    this.setState({
+      person:person
+    });
+    const action = { type:'setCity', value:person}
+    store.dispatch(action);
+  }
+  componentDidMount(){
+    this.setState(prevState=>({
+      person:prevState.person
+    }))
+  }
+  // componentWillUpdate(nextState){
+  //     this.setState(nextState=>({
+  //       person:nextState.person
+  //     }))
+  //     console.log(`person: ${nextState.person}`)
+  // }
   render() {
     return (
       <div className="App">
@@ -13,12 +41,16 @@ class App extends Component {
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+           
+            onClick={this.hanglePushPerson}
           >
-            Learn React
+           click
           </a>
+          <ul>
+              {this.state.person.map(name =>{
+                  return <li>{name}</li>
+              })}
+          </ul>
         </header>
       </div>
     );
